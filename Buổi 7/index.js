256a7b0f4b92b926f09f65c7a00c18f20714859d
thangkma99 <thangkma.work@gmail.com>
2021-04-21 15:05:15 +0700

    up

---
+++ Buổi 7/index.js
@@ -0,0 +1,27 @@
+console.log("Vu Minh Thang");
+// 
+let a = prompt('Nhập a')
+let b = prompt('Nhập b')
+let c = prompt('Nhập c')
+
+if (a == 0) {
+    if (b == 0) {
+        if (c == 0) {
+            console.log('Phuong trinh vo so nghiem')
+        }
+    } else {
+        console.log('Phuong trinh co nghiem duy nhat: ' + (-c / b))
+    }
+} else {
+    let delta = b * b - 4 * a * c;
+    if (delta > 0) {
+        let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        console.log('Phương trình có 2 nghiệm phân biệt:\n x1 = ' + x1 + '\n x2 = ' + x2)
+    } else if (delta == 0) {
+        let result = -b / 2 * a
+        console.log('Phuong trinh co nghiem kep: x1 = x2 = ' + result)
+    } else {
+        console.log('Phuong trinh vo nghiem')
+    }
+}
